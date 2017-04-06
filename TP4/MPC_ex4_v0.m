a66df4c8fcf5537af7489c4d65418357b130c959
hubernikus <hubernikus@gmail.com>
2017-04-06 14:42:15 +0200

    initial file

--- TP4/MPC_ex4_v0.m
+++ TP4/MPC_ex4_v0.m
@@ -11,7 +11,7 @@ clear variables; close all; clear all;
 
 %%
 A = [0.9752, 1.4544; -0.0327, 0.9315];
-b = [0.0248; 0.0327];
+B = [0.0248; 0.0327];
 
 f_nat = 0.15; % [r/s] Natural frquency
 
@@ -20,6 +20,20 @@ discRate = 1.5; % [r/s] Discretization rate applied
 xi = 0.1; % Dampin Ratio
 
 N = 10; % Horizon length
+
+
+
+sys = LTISystem('A',A,'B',B);
+
+% Define limits
+sys.x.min = [5, 0.2];
+sys.x.max = [-5, -0.2]';
+
+sys.x.penalty = QuadFunction(Q);
+
+%sys.LGRGain = sys.LQRPenalty.weight sys.LQRSet
+ls
+
 
 
 
