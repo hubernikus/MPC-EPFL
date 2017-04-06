7908068291e6ca75b3732ce521e2d2d2cadaa907
hubernikus <hubernikus@gmail.com>
2017-04-06 15:03:00 +0200

    Change input penalty

--- TP4/MPC_ex4_v0.m
+++ TP4/MPC_ex4_v0.m
@@ -10,11 +10,13 @@
 clear variables; close all; clear all;
 
 %%
+clc;
+
 A = [0.9752, 1.4544; -0.0327, 0.9315];
 B = [0.0248; 0.0327];
 
 Q = [10 0; 0 10];
-R = [1 0; 0 1];
+R = [1];
 
 f_nat = 0.15; % [r/s] Natural frquency
 
@@ -34,14 +36,12 @@ sys.x.max = [5, 0.2];
 sys.u.min = -1.75;
 sys.u.max = 1.75;
 
+sys.x.penalty = QuadFunction(Q)
+sys.u.penalty = QuadFunction(R)
 
-
-sys.x.penalty = QuadFunction(Q);
-
-LQRGarin = sys.LGRGain;
+LQRGarin = sys.LQRGain;
 LQRPenalty = sys.LQRPenalty.weight;
 LQRSet = sys.LQRSet;
-
 
 
 fprintf('Programm terminated. \n')
