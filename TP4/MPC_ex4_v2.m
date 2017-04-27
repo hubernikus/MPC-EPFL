7efa2838c18575c75a372ba16a7f706be060be40
hubernikus <hubernikus@gmail.com>
2017-04-27 14:40:43 +0200

    Ex 5

--- TP4/MPC_ex4_v2.m
+++ TP4/MPC_ex4_v2.m
@@ -45,33 +45,38 @@ sys.x.penalty = QuadFunction(Q);
 sys.u.penalty = QuadFunction(R);
 
 LQRGarin = sys.LQRGain
-LQRPenalty = sys.LQRPenalty.weight
-LQRSet = sys.LQRSet
-
+Q_f =  sys.LQRPenalty.weight
+F_f = sys.LQRSet.A
+f_f = sys.LQRSet.b
 
 % Ex2 Matlab optimization
 %z = [x;u];
 %H = blkdiag(kron(eye(N???1),Q), Qf, kron(eye(N),R))
 
 % Optimization
-H=blkdiag(kron(eye(N),Q),kron(eye(N),R));
+H=blkdiag(kron(eye(N-1),Q),Q_f,kron(eye(N),R));
 %H= [Q,zeros(dimX,dimU);zeros(dimU,dimX) R];
-h = zeros(N*(dimX+dimY),1);
+h = zeros(N*(dimX+dimU),1);
 
 % Define Matrizes for comparison restriction
-g = [kron(ones(N,1),[5 -5 0.2 -0.2]'); kron(ones(N,1),[1.75 -1.75]')];
-G = blkdiag(kron(eye(N),[1 0; -1 0; 0 1; 0 -1]), ...
-            kron(eye(N),[1;-1]))
+F  = [1 0; -1 0; 0 1; 0 -1]; f = [5 5 0.2 0.2]';
+M = [1;-1]; m = [1.75 1.75]';
+
+G = blkdiag(kron(eye(N-1), F), F_f,kron(eye(N), M));
+g = [kron(ones(N-1,1),f);f_f; kron(ones(N,1),m)];
+x0 = [3 0]'; % Starting 
 
 % Create Equality matrizes Aeq and beq
-T = [eye(N*dimX) + kron(diag(ones(1,N-1),-1),A),  kron(diag(ones(1,N)),B)];
-t = [A; zeros(dimX*(N-1),dimX)];
+T = [eye(N*dimX) + kron(diag(ones(1,N-1),-1),-A),  kron(diag(ones(1,N)),B)];
+t = [A; zeros(dimX*(N-1),dimX)]*x0;
 
+fprintf('Start optimization \n')
+options = optimoptions(@quadprog,'ConstraintTolerance',1e0)
+[zopt, fval, flag] = quadprog(H, h, G, g, T, t,[],[],[],options)
 
+%'ConstraintTolerance',1e-8)
+%                                                ConstraintTolerance            
 
-
-LQRGarin = sys.LQRGain;
-LQRPenalty = sys.LQRPenalty.weight;
-LQRSet = sys.LQRSet;
 
 fprintf('Programm terminated. \n')
+
---
+++ TP5/MPC_ex5_v1.m
@@ -0,0 +1,15 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%            Model Predictive Control - Exercise 5
+%              EPFL - Spring semester 2017 - 
+%
+%            Huber Lukas - Zgraggen Jannik
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear variables; close all; clear all;
+
+addpath(genpath('../tbxmanager'))
+
+%%
+close all;
+
