2855c6b4aa31aab3154dd063727d81bf7dd2e46a
hubernikus <hubernikus@gmail.com>
2017-05-04 14:52:44 +0200

    Update estimator Function

--- TP5/estimatorMPC.m
+++ TP5/estimatorMPC.m
@@ -2,3 +2,5 @@ function [x_hat2, d_hat2] estimatorMPC(A_err, B_err, L, C_err, u, y, x_hat, d_ha
 %ESTIMATORMPC - estimation of the funciton
 
 dh_hat2 = A_err*[x_hat; d_hat]+B_err*u+L*[C_err,y];
+x_hat2 = dh_hat2(1:2);
+d_hat2 = dh_hat2(3);
